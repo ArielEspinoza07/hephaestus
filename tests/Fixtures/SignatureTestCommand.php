15dcdec5f5e6101bb0d3911c7db7d2a10e4aa59c
Ariel Espinoza <arielespinoza46@gmail.com>
2026-02-27 04:35:53 -0600

    feat(test): add fixture

--- tests/Fixtures/SignatureTestCommand.php
+++ tests/Fixtures/SignatureTestCommand.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Hephaestus\Tests\Fixtures;
 
+use Hephaestus\Attributes\Input;
+use Hephaestus\Attributes\Output;
 use Hephaestus\Attributes\Signature;
 
 #[Signature('app:test')]
+#[Input]
+#[Output]
 final class SignatureTestCommand {}
