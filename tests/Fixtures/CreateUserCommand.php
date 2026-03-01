ddc4d672ff296091c5a64b1921efadd8535f4212
Ariel Espinoza <arielespinoza46@gmail.com>
2026-03-01 12:29:17 -0600

    feat(tests): add attribute to fixture

--- tests/Fixtures/CreateUserCommand.php
+++ tests/Fixtures/CreateUserCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Hephaestus\Tests\Fixtures;
 
+use Hephaestus\Attributes\Alias;
 use Hephaestus\Attributes\Description;
 use Hephaestus\Attributes\CompositeInput;
 use Hephaestus\Attributes\Output;
@@ -12,6 +13,7 @@ use Hephaestus\Console\Command;
 
 #[Signature('app:create-user')]
 #[Description('Creates a new user')]
+#[Alias('app:create-user|app:add-user|app:new-user')]
 #[Output]
 final readonly class CreateUserCommand extends Command
 {
