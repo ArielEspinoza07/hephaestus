73b7143443181d3168663bdd94f56d3191fa356d
Ariel Espinoza <arielespinoza46@gmail.com>
2026-03-01 10:06:06 -0600

    feat(metadata): add OptionAttributeResolver

---
+++ src/Metadata/Resolver/OptionAttributeResolver.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hephaestus\Metadata\Resolver;
+
+use Hephaestus\Attributes\Option;
+use Hephaestus\Metadata\Support\OptionMetadata;
+use ReflectionAttribute;
+use ReflectionNamedType;
+use ReflectionParameter;
+
+/**
+ * @template T of object
+ */
+final class OptionAttributeResolver
+{
+    public function resolve(ReflectionParameter $parameter): OptionMetadata
+    {
+        /** @var ReflectionAttribute<T> $attribute */
+        $attribute = array_filter(
+            array: $parameter->getAttributes(),
+            callback: fn (ReflectionAttribute $attribute) => $attribute->getName() === Option::class,
+        ) |> array_first(...);
+
+        /** @var Option $option */
+        $option = $attribute->newInstance();
+        /** @var ReflectionNamedType $parameterType */
+        $parameterType = $parameter->getType();
+        $type = mb_strtolower($parameterType->getName());
+
+        return new OptionMetadata(
+            name: $parameter->getName(),
+            type: $type,
+            description: $option->description,
+            acceptValue: $type === 'bool' ? true : $option->acceptValue,
+            default: $parameter->isDefaultValueAvailable() ? $parameter->getDefaultValue() : $option->default,
+            shortcut: $option->shortcut,
+        );
+    }
+}
