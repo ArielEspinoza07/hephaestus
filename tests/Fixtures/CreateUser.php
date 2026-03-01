816e626d4853173e048422b25f5df5ef83c08ea4
Ariel Espinoza <arielespinoza46@gmail.com>
2026-03-01 10:13:48 -0600

    feat(tests): add option

--- tests/Fixtures/CreateUser.php
+++ tests/Fixtures/CreateUser.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Hephaestus\Tests\Fixtures;
 
 use Hephaestus\Attributes\Argument;
+use Hephaestus\Attributes\Option;
 
 final readonly class CreateUser
 {
@@ -13,5 +14,7 @@ final readonly class CreateUser
         public string $name,
         #[Argument(description: 'The email address of the user to create')]
         public string $email,
+        #[Option(description: 'Whether the user should be an admin', shortcut: 'a')]
+        public bool $isAdmin = false,
     ) {}
 }
